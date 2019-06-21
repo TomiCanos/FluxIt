6865a270261e5962ad0939afbc3890ca98d9c2e5
Tomi <tomas.canosa@hotmail.com>
2019-06-20 21:44:53 -0300

    navigation bar XML
    moving code from main activity to specific fragments to implement navigation bar function

--- app/src/main/java/com/example/fluxitdemo/view/MainActivity.java
+++ app/src/main/java/com/example/fluxitdemo/view/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.fluxitdemo.R;
 import com.example.fluxitdemo.model.DAOProduct;
@@ -19,7 +22,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
     private EditText editText;
     private RecyclerView recyclerView;
     private DAOProduct daoProduct;
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         daoProduct = new DAOProduct();
         productAdapter = new ProductAdapter(new ArrayList<Product>());
 
@@ -36,26 +38,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setview() {
-        button = findViewById(R.id.button_search);
         editText = findViewById(R.id.edittxt_search_query);
         recyclerView = findViewById(R.id.recycler_view_result_products);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(productAdapter);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View v) {
-
-                daoProduct.searchProducts(editText.getText().toString(),
-                        new ResultListener<List<Product>>() {
-                            @Override
-                            public void finish(List<Product> results) {
-                                productAdapter.dataSetChanged(results);
-                            }
-                        })
-                ;
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch();
+                    return true;
+                }
+                return false;
             }
         });
+    }
+
+    private void performSearch() {
+        editText.clearFocus();
+        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        in.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+
+        daoProduct.searchProducts(
+                editText.getText().toString(),
+                new ResultListener<List<Product>>() {
+                    @Override
+                    public void finish(List<Product> results) {
+                        productAdapter.dataSetChanged(results);
+                    }
+                })
+        ;
+
     }
 }
---
+++ app/src/main/java/com/example/fluxitdemo/view/SearchFragment.java
@@ -0,0 +1,62 @@
+package com.example.fluxitdemo.view;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fluxitdemo.R;
+
+public class SearchFragment extends Fragment {
+
+//    private OnFragmentInteractionListener mListener;
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+
+
+        return view;
+    }
+
+
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+//
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
+}
